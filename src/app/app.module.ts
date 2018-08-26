f00f1b91031497e27b6a51d50d74417ba8f60375
Rebekah <rebmid@gmail.com>
2018-08-26 12:39:00 -0700

    Weather app changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import {WeatherService } from './weather/weather.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CurrentWeatherComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
