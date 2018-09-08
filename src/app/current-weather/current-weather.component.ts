96954243fd46fe9bb8d33b0dc281a5f116764f98
Rebekah <rebmid@gmail.com>
2018-09-07 23:59:19 -0700

    backed out to dummy data

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -9,12 +9,39 @@ import { WeatherService } from '../weather/weather.service';
 })
 export class CurrentWeatherComponent implements OnInit {
   current: ICurrentWeather;
+     // (private weatherService: WeatherService) //
 
-    constructor(private weatherService: WeatherService) {
-    }
+    constructor() {
+     this.current = {
+       city: 'Bethasda',
+       country: 'US',
+       date: new Date(),
+       image: '',
+       temperature: 72,
+       description: 'sunny',
+     };
+     }
+         // this.weatherService.getCurrentWeather('Bethesda', 'US').subscribe((data) => this.current = data);//
 
   ngOnInit() {
-    this.weatherService.getCurrentWeather('Bethesda', 'US').subscribe((data) => this.current = data);
   }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
